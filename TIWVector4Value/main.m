77a55aaf286fa51e543b77b810c91339ae8c51ed
Jules Looker <Jules.Looker@me.com>
2011-07-22 12:44:53 +0100

    Initial Commit

---
+++ TIWVector4Value/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TIWVector4Value
+//
+//  Created by Jules Looker on 22/07/2011.
+//  Copyright 2011 The Interface Works. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
